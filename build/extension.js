89b8de3db8d04575c2d4dd5e4da73c15514be4e9
W3Dojo <W3Dojo@Gmail.com>
2022-02-18 14:32:26 -0800

    added './theme/templates' dir, with 3 templates (1) - Static (2) - hz workbench borders (3) - vert workbench borders

--- build/extension.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const uidynamics_parser_1 = require("./lib/uidynamics-parser");
-(0, uidynamics_parser_1.parseJSONFile)(`${process.cwd()}/.tmp/colorTheme.json`).then((json) => {
-    console.log(`\n {\n    "focusBorder": ${json.focusBorder}\n}\n\n`);
-    json.focusBorder = '#00FF00';
-    console.log(`\n {\n    "focusBorder": ${json.focusBorder}\n}\n\n`);
-});
